2797a9e167e91c64b4928ba864732a75dcf6ac91
anildegirmenci <c1211016@student.cankaya.edu.tr>
2020-01-25 17:50:33 +0300

    cmt

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className="header">
+      <img className = "Otsimo_PNG" src = "otsimo.png" width ="300" height = "160"/>
+      <h1>React Quiz for Otsimo</h1>
+      </div>
+  )
+}
