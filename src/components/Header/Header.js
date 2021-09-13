c1aa536470a9b4fcd6033c10adca11b2b1e9bf50
anildegirmenci <anildegirmenci71@gmail.com>
2021-09-13 14:20:27 +0300

    ReadMe.md has added, some styles has been changed.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default () => {
   return (
     <div className="header">
-      <img className = "Otsimo_PNG" src = "otsimo.png" width ="300" height = "160"/>
-      <h1>React Quiz for Otsimo</h1>
+      <img className = "imgOtsimo" src = "otsimo.png" width ="300" height = "160"/>
+      <h1>ReactJS Quiz App!</h1>
       </div>
   )
 }
